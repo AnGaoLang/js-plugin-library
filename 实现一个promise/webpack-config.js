ffe37cf396a6ff8a9e9d2b7331db48ec7a383ffb
AnGaoLang <1774087845@qq.com>
2019-05-20 18:05:45 +0800

    11

--- 实现一个promise/webpack-config.js
+++ 实现一个promise/webpack-config.js
@@ -10,8 +10,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     libraryExport: "default", // 对外暴露default属性，就可以直接调用default里的属性(针对script引入方式!必须)
     library: '[name]',  // 指定类库名,将类库暴露的方法指定给改标识符.主要用于直接引用的方式(比如使用script 标签)
-    globalObject: 'this', // 定义全局变量,兼容node和浏览器运行，避免出现"window is not defined"的情况(node环境)
-    libraryTarget: "umd" // 定义打包方式Universal Module Definition,同时支持在CommonJS、AMD和全局变量使用
+    globalObject: 'this', // 定义全局变量,兼容node和浏览器运行，避免出现"window is not defined"的情况(兼容node环境,script引入方式可省略)
+    libraryTarget: "umd" // 定义打包方式,同时支持在CommonJS、AMD和全局变量(不支持es6module)使用,(script引入方式可省略)
   },
   devtool: "",
   resolve: {
