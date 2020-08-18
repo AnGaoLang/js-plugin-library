003a201dda11a3cc44093e2ad434caae4adc5570
anGaoLang <1774087845@qq.com>
2020-08-18 21:29:10 +0800

    深拷贝

--- 基础工具函数/fundamental.js
+++ 基础工具函数/fundamental.js
@@ -388,3 +388,25 @@ blobToDataURL (blob, callback) {
   a.onload = function (e) { callback(e.target.result); }
   a.readAsDataURL(blob);
 }
+
+// 校验数据类型及深拷贝
+function  judgeType(obj) {
+  // tostring会返回对应不同的标签的构造函数
+  const toString = Object.prototype.toString;
+  const map = {
+    '[object Boolean]': 'boolean',
+    '[object Number]': 'number',
+    '[object String]': 'string',
+    '[object Function]': 'function',
+    '[object Array]': 'array',
+    '[object Date]': 'date',
+    '[object RegExp]': 'regExp',
+    '[object Undefined]': 'undefined',
+    '[object Null]': 'null',
+    '[object Object]': 'object',
+  };
+  if (obj instanceof Element) {
+    return 'element';
+  }
+  return map[toString.call(obj)];
+}
